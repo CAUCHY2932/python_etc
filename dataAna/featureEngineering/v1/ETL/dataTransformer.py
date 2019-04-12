1b07da42d9c821b1b1ce7e10e6d31fd08a528a4f
young <2932045582@qq.com>
2019-04-12 18:02:20 +0800

    add tansuo files

---
+++ dataAna/featureEngineering/v1/ETL/dataTransformer.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+"""
+    2019/4/12 11:42 by young
+"""
+
+
+class DataTransformer:
+    """
+    TODO 数据转换，包含：
+
+    """
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def read_file(self):
+        pass
---
+++ dataAna/featureEngineering/v1/re_demo.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+"""
+    2019/4/12 15:42 by young
+"""
+
+import re
+
+demo_str = """
+928739hsjfo@qq.com
+
+
+"""
+
+# 日期
+
+
