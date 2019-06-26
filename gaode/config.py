8803daf187c4fbe5afee7d7c294e85d3a093803b
young <2932045582@qq.com>
2019-06-27 00:20:09 +0800

    add mutilprocess

---
+++ gaode/config.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+"""
+    :author: young
+    :DATE: 2019-06-26 21:37
+    :copyright: © 2019 young <haochen2932@foxmail.com>
+    :license: None, see LICENSE for more details.
+"""
+
+KEY_1 = ''
+KEY_2 = ''
+BASE_GAODE_URL = ""
+BAIDU_AK = ''
+PROCESS_NUM = 3
+CHUNK_SIZE = 100
---
+++ gaode/new_2013_2018/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+"""
+    :author: young
+    :DATE: 2019-06-26 21:44
+    :copyright: © 2019 young <haochen2932@foxmail.com>
+    :license: None, see LICENSE for more details.
+"""
+
+
+
+
+
+
+
