522962903b1be90984f4407bb5b5bcbde9551919
young <2932045582@qq.com>
2019-07-17 09:35:30 +0800

    change some files

---
+++ s4_application/反向生成实体类2.py
@@ -0,0 +1,7 @@
+# pip install sqlacodegen
+
+
+# 运行命令
+"""
+sqlacodegen mysql+pymysql://jnctpm:Jiannanchun123@jnctpm.cotpwdfxy0tl.rds.cn-northwest-1.amazonaws.com.cn:6033/jnc_tpm_dev?charset=utf8 > models.py
+"""
