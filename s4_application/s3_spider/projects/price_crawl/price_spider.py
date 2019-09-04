6c1a18b7d604af7aad99cf277cd5f02e7d281e44
young <2932045582@qq.com>
2019-09-04 18:16:31 +0800

    # change files and del files2

--- s4_application/s3_spider/projects/price_crawl/price_spider.py
+++ s4_application/s3_spider/projects/price_crawl/price_spider.py
@@ -7,24 +7,24 @@ created by young on 2018/12/25 17:05
     import the require module
 """
 
-from price_crawl import spider_class as spder
-from price_crawl import setting
+from . import spider_class as spder
+from . import setting
 
 __author__ = 'young'
 
 
-def main():
+def main(pattern_mode):
     crawl_spider = spder.Crawl_price()
     for item in setting['SITE_LIST']:
         html_text = crawl_spider.get_html_source_code(item)
         if html_text is None:
             continue  # 出口一：
-        order_num = crawl_spider.select_mode(link_list=setting['LINK_LIST'], item)
+        order_num = crawl_spider.select_mode(link_list=setting['LINK_LIST'], site=item)
         if order_num == -1:
             str_taget = ''
         else:
             obj = crawl_spider.htmlish(html_text)
-            str_target = crawl_spider.regex_parse(pattern_mode, text)
+            str_target = crawl_spider.regex_parse(pattern_mode,text=obj)
         crawl_spider.write_to_file(str_taget)
 
 
--- s4_application/s3_spider/projects/spider_netease/netease.py
+++ s4_application/s3_spider/projects/spider_netease/netease.py
@@ -1,7 +1,6 @@
-__author__='young'
+__author__ = 'young'
 
 import requests
-from lxml import etree
 
 
 # 业务需求：输入关键词，搜索出歌曲，然后供人试听，并免费下载
@@ -10,43 +9,42 @@ from lxml import etree
 # 定义http头
 
 class Spider_netease:
-    def __init__:
-        self.keyword=''
-        self.songs=[] # 由元组组成的列表
+    def __init__(self):
+        self.keyword = ''
+        self.songs = []  # 由元组组成的列表
+        self.content = ''
 
-    def _fetch_content(url,headers):
-        r=requests.get(url,headers)
-        if r.status_code==200:
-            r.encoding=r.apparent_encoding
+    def _fetch_content(url, headers):
+        r = requests.get(url, headers)
+        if r.status_code == 200:
+            r.encoding = r.apparent_encoding
             return r
         return None
 
-    def _fetch_text(resp):
+    def _fetch_text(self, resp):
         if resp:
             return resp.text
         return None
 
-    def _fetch_binary(resp):
+    def _fetch_binary(self, resp):
         if resp:
             return resp.content
         return None
 
-    def _fetch_json():
+    def _fetch_json(self, resp):
         if resp:
-            return resp.json
+            return resp.json()
         return None
 
-    def _analysis():
+    def _analysis(self):
         pass
 
-    def _persistent():
+    def _persistent(self):
+        pass
+
+    def go_crawl(self):
         pass
 
 
-    def go_crawl():
-        pass
-
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     pass
