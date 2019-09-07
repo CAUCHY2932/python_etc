b59c70d5302f5997a79155e2762e3db7a88df8af
young <2932045582@qq.com>
2019-09-07 23:12:19 +0800

    add bing file

---
+++ s3_tasks/download_bing_pic/tst01.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+import requests
+
+
+class Crawlpy:
+    pass
+
+
+class Spder:
+    def __init__(self):
+        pass
+    pass
+
+class Item:
+    def __init__(self):
+        pass
+    pass
+
+class Pipeline:
+    def __init__(self):
+        pass
+    pass
+
+class Midware:
+    def __init__(self):
+        pass
+
+    pass
+
+
+if __name__ == "__main__":
+    pass
