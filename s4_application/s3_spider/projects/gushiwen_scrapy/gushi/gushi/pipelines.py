662ccab1de2ae73d12ebe8357d228ab596b1f52e
young <2932045582@qq.com>
2019-10-29 17:43:28 +0800

    add file

--- s4_application/s3_spider/projects/gushiwen_scrapy/gushi/gushi/pipelines.py
+++ s4_application/s3_spider/projects/gushiwen_scrapy/gushi/gushi/pipelines.py
@@ -6,6 +6,10 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import time
+# from gushi import settings as s
+
+from scrapy.utils.project import get_project_settings
+s = get_project_settings()
 
 import psycopg2
 
@@ -14,15 +18,23 @@ class GushiPipeline(object):
     def __init__(self):
         self.conn = psycopg2.connect(database="mydb",
                                      user="postgres",
-                                     password="12345678",
+                                     password="123456",
                                      host="127.0.0.1",
                                      port="5432")
+        # self.conn = psycopg2.connect(database=s.get('database_name'),
+        #                              user=s.get("database_user"),
+        #                              password=s.get("database_password"),
+        #                              host=s.get("database_url"),
+        #                              port='5432')
         self.cur = self.conn.cursor()
         pass
+
 
     def process_item(self, item, spider):
         insert_sql = f"""insert into poets(name, content)values ('{item["name"]}', '{item["content"]}')
         """
+        # insert_sql = f"""insert into {s["table_name"]}(name, content)values ('{item["name"]}', '{item["content"]}')
+        # """
         self.cur.execute(insert_sql)
         self.conn.commit()
         # t = int(time.time())
